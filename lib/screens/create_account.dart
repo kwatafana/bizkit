db26d49c5365223563fcb4336c9bce21231e717e
Jackson G. Kaindume <cy6erlion@protonmail.com>
2023-08-13 09:53:59 +0200

    [ui] refactor and use a menu

--- lib/screens/create_account.dart
+++ lib/screens/create_account.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:bizkit/wid/scaffold.dart';
 import 'package:kong/kong.dart';
+import 'package:bizkit/wid/menu.dart';
 
 /// Create Account Screen
 class CreateAccountScreen extends StatefulWidget {
@@ -24,148 +25,146 @@ class _AddProjectScreenState extends State<CreateAccountScreen> {
 
   @override
   Widget build(BuildContext context) {
-    TextEditingController usernameController = TextEditingController();
-    TextEditingController emailController = TextEditingController();
-    TextEditingController passwordController = TextEditingController();
-    TextEditingController retypedPasswordController = TextEditingController();
+    Row title = Row(children: <Widget>[
+      Container(
+        margin: const EdgeInsets.only(right: 5),
+        child: const Icon(Icons.cookie),
+      ),
+      const Text("BizKit | create account")
+    ]);
 
-    return bizkitScaffold(
-        Row(children: <Widget>[
-          Container(
-            margin: const EdgeInsets.only(right: 5),
-            child: const Icon(Icons.cookie),
-          ),
-          const Text("BizKit | create account")
-        ]),
-        ListView(children: <Widget>[
-          Container(
-              margin: const EdgeInsets.only(top: 30),
-              child:
-                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                Container(
-                  margin: const EdgeInsets.only(right: 5),
-                  child: const Icon(Icons.person, color: Colors.white),
-                ),
-                const Text(
-                  "New Account",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                  ),
-                ),
-              ])),
-          Container(
-            padding: const EdgeInsets.all(5),
-            child: const Divider(color: Colors.tealAccent),
-          ),
-          Container(
-            padding: const EdgeInsets.all(10),
-            child: TextField(
-              controller: usernameController,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: "Username",
-                labelStyle: TextStyle(color: Colors.grey),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.teal),
-                ),
-              ),
-              style: const TextStyle(color: Colors.white),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(10),
-            child: TextField(
-              controller: emailController,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: "Email",
-                labelStyle: TextStyle(color: Colors.grey),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.teal),
-                ),
-              ),
-              style: const TextStyle(color: Colors.white),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(10),
-            child: TextField(
-              controller: passwordController,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: "Password",
-                labelStyle: TextStyle(color: Colors.grey),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.teal),
-                ),
-              ),
-              obscureText: true,
-              style: const TextStyle(color: Colors.white),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(10),
-            child: TextField(
-              controller: retypedPasswordController,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: "Re-type Password",
-                labelStyle: TextStyle(color: Colors.grey),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.teal),
-                ),
-              ),
-              obscureText: true,
-              style: const TextStyle(color: Colors.white),
-            ),
-          ),
-          Container(
-              height: 50,
-              padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-              margin: const EdgeInsets.only(top: 10.0),
-              child: ElevatedButton(
-                child: const Text("create account"),
-                onPressed: () async {
-                  final username = usernameController.text;
-                  final email = emailController.text;
-                  final password = passwordController.text;
-                  final retypedPassword = retypedPasswordController.text;
-
-                  if (username.isNotEmpty &&
-                      email.isNotEmpty &&
-                      password.isNotEmpty) {
-                    if (retypedPassword == password) {
-                      try {
-                        final account_input = AccountCreationInput.validCreate(
-                            username, email, password);
-                        final public_account_data =
-                            await kong.create(account_input);
-
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(
-                              content: Text(
-                                  "Account for ${public_account_data.username} created!")),
-                        );
-                        Navigator.pushNamed(context, '/login');
-                      } catch (e) {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(content: Text(e.toString())),
-                        );
-                      }
-                    } else {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text('Password mismatch')),
-                      );
-                      return;
-                    }
-                  } else
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text('Fill in all fields')),
-                    );
-                  return;
-                },
-              )),
-        ]));
+    return bizkitScaffold(title, body(context, kong), bizkitMenu(context, ""));
   }
+}
+
+Widget body(context, kong) {
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController retypedPasswordController = TextEditingController();
+
+  return ListView(children: <Widget>[
+    Container(
+      margin: const EdgeInsets.only(right: 5),
+      child: const Icon(Icons.person, color: Colors.white),
+    ),
+    const Text(
+      "New Account",
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 20,
+      ),
+    ),
+    Container(
+      padding: const EdgeInsets.all(5),
+      child: const Divider(color: Colors.tealAccent),
+    ),
+    Container(
+      padding: const EdgeInsets.all(10),
+      child: TextField(
+        controller: usernameController,
+        decoration: const InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: "Username",
+          labelStyle: TextStyle(color: Colors.grey),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.teal),
+          ),
+        ),
+        style: const TextStyle(color: Colors.white),
+      ),
+    ),
+    Container(
+      padding: const EdgeInsets.all(10),
+      child: TextField(
+        controller: emailController,
+        decoration: const InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: "Email",
+          labelStyle: TextStyle(color: Colors.grey),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.teal),
+          ),
+        ),
+        style: const TextStyle(color: Colors.white),
+      ),
+    ),
+    Container(
+      padding: const EdgeInsets.all(10),
+      child: TextField(
+        controller: passwordController,
+        decoration: const InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: "Password",
+          labelStyle: TextStyle(color: Colors.grey),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.teal),
+          ),
+        ),
+        obscureText: true,
+        style: const TextStyle(color: Colors.white),
+      ),
+    ),
+    Container(
+      padding: const EdgeInsets.all(10),
+      child: TextField(
+        controller: retypedPasswordController,
+        decoration: const InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: "Re-type Password",
+          labelStyle: TextStyle(color: Colors.grey),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.teal),
+          ),
+        ),
+        obscureText: true,
+        style: const TextStyle(color: Colors.white),
+      ),
+    ),
+    Container(
+        height: 50,
+        padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+        margin: const EdgeInsets.only(top: 10.0),
+        child: ElevatedButton(
+          child: const Text("create account"),
+          onPressed: () async {
+            final username = usernameController.text;
+            final email = emailController.text;
+            final password = passwordController.text;
+            final retypedPassword = retypedPasswordController.text;
+
+            if (username.isNotEmpty &&
+                email.isNotEmpty &&
+                password.isNotEmpty) {
+              if (retypedPassword == password) {
+                try {
+                  final account_input = AccountCreationInput.validCreate(
+                      username, email, password);
+                  final public_account_data = await kong.create(account_input);
+
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                        content: Text(
+                            "Account for ${public_account_data.username} created!")),
+                  );
+                  Navigator.pushNamed(context, '/login');
+                } catch (e) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text(e.toString())),
+                  );
+                }
+              } else {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('Password mismatch')),
+                );
+                return;
+              }
+            } else
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text('Fill in all fields')),
+              );
+            return;
+          },
+        ))
+  ]);
 }
