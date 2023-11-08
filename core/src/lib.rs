e460ca5d0948deb7d4c298b84007d92fadcc76bf
Jackson G. Kaindume <cy6erlion@protonmail.com>
2023-11-08 14:27:56 +0200

    [core]: remove uneeded code

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,14 +1,2 @@
 #![warn(missing_docs, unreachable_pub, future_incompatible, rust_2018_idioms)]
-
 pub mod inventory;
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
