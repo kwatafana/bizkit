b85b773f1579d58b0588cdd0ef52e143ffb2cc99
Jackson G. Kaindume <cy6erlion@protonmail.com>
2024-02-01 13:20:30 +0200

    [crates/data]: Move StaffStatus code to its own file

--- crates/data/src/lib.rs
+++ crates/data/src/lib.rs
@@ -4,10 +4,12 @@
 #![doc(html_logo_url = "https://bizkit.kwatafana.org/logo.jpeg")]
 #![warn(missing_docs, unreachable_pub, future_incompatible, rust_2018_idioms)]
 
-pub use staff::{Staff, StaffStatus};
+pub use staff::Staff;
+pub use staff_status::StaffStatus;
 pub use product::Product;
 pub use error::Error;
 
 mod product;
 mod staff;
 mod error;
+mod staff_status;
---
+++ crates/data/src/staff_status.rs
@@ -0,0 +1,89 @@
+use serde::{Deserialize, Serialize, de::Deserializer, ser::Serializer};
+use crate::Error;
+use std::fmt;
+use std::str::FromStr;
+use rusqlite::types::{ToSql, ToSqlOutput, FromSql, ValueRef, FromSqlResult, FromSqlError};
+
+/// The working status of a staff memeber
+#[derive(PartialEq, Debug, Serialize, Deserialize)]
+pub enum StaffStatus{
+    /// Active staff member
+    Active,
+    /// Retired staff member
+    Retired,
+    /// Resigned staff member
+    Resigned,
+    /// Absent staff member
+    Absent,
+    /// A staff memeber on holiday
+    Holiday,
+    /// A staff memeber on leave
+    Leave,
+    /// A staff memeber on vacation
+    Vacation
+}
+
+impl StaffStatus{
+    /// Deserialize
+    pub fn deserialize <'de, D>(deserializer: D) -> Result<StaffStatus, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let buf = String::deserialize(deserializer)?;
+	// TODO: don't use unwrap
+        let out: StaffStatus = buf.parse::<StaffStatus>().unwrap();
+	Ok(out)
+
+    }
+
+    /// Serialize
+    pub fn serialize<S>(staff_status: &StaffStatus, serializer: S) -> Result<S::Ok, S::Error> where S: Serializer {
+	serializer.serialize_str(&staff_status.to_string())
+    }
+}
+
+impl fmt::Display for StaffStatus {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+	let status = match self{
+	    StaffStatus::Active => "Active",
+	    StaffStatus::Retired => "Retired",
+	    StaffStatus::Resigned => "Resigned",
+	    StaffStatus::Absent => "Absent",
+	    StaffStatus::Holiday => "Holiday",
+	    StaffStatus::Leave => "Leave",
+	    StaffStatus::Vacation => "Vacation",
+
+	};
+        writeln!(f, "{status}")
+    }
+}
+
+impl FromStr for StaffStatus {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+	match s{
+            "Active" => Ok(StaffStatus::Active),
+	    "Retired" => Ok(StaffStatus::Retired),
+	    "Resigned" => Ok(StaffStatus::Resigned),
+	    "Absent" => Ok(StaffStatus::Absent),
+	    "Holiday" => Ok(StaffStatus::Holiday),
+	    "Leave" => Ok(StaffStatus::Leave),
+	    "Vacation" => Ok(StaffStatus::Vacation),
+	    _ => Err(Error::StaffStatus),
+	}
+    }
+}
+
+impl ToSql for StaffStatus {
+    fn to_sql(&self) -> rusqlite::Result<ToSqlOutput<'_>> {
+        Ok(self.to_string().into())
+    }
+}
+
+impl FromSql for StaffStatus {
+    fn column_result(value: ValueRef<'_>) -> FromSqlResult<Self> {
+	value.as_str()?.parse()
+            .map_err(|e| FromSqlError::Other(Box::new(e)))
+    }
+}
