3ba05abc305f90f0ef86ac059e5f8bac122529e3
Jackson G. Kaindume <cy6erlion@protonmail.com>
2024-02-01 10:56:55 +0200

    [crates/database]: Explicit reexporting

--- crates/database/src/lib.rs
+++ crates/database/src/lib.rs
@@ -4,8 +4,10 @@
 #![doc(html_logo_url = "https://bizkit.kwatafana.org/logo.jpeg")]
 #![warn(missing_docs, unreachable_pub, future_incompatible, rust_2018_idioms)]
 
-pub mod inventory;
+pub use inventory::InventoryDatabase;
 pub use error::Error;
 pub use staff::Database;
+
+mod inventory;
 mod staff;
 mod error;
