16e79a79b3b6f25e1397ef2ba6900212eb3cdc2d
Jackson G. Kaindume <cy6erlion@protonmail.com>
2024-02-01 10:52:19 +0200

    [crates/database]: Add error.rs

--- crates/database/src/inventory.rs
+++ crates/database/src/inventory.rs
@@ -1,7 +1,6 @@
 //! bizkit -- Inventory management
 
 use rusqlite::{params, Connection};
-use std::{fmt, path::PathBuf};
 use bizkitdata::product::Product;
 
 mod sql {
@@ -235,44 +234,6 @@ impl InventoryDatabase {
     }
 }
 
-#[derive(Debug)]
-/// error management
-pub enum Error {
-    /// Database connection error
-    Connection,
-    /// Database transaction error
-    Transaction,
-    /// Database table creation error
-    TableCreation,
-    /// Database field error
-    Field,
-    /// SQL error
-    SQL,
-}
-
-impl std::error::Error for Error {}
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Self::Connection => {
-                write!(f, "Database connection")
-            }
-            Self::Transaction => {
-                write!(f, "Database transaction error")
-            }
-            Self::TableCreation => {
-                write!(f, "Database table creation error")
-            }
-            Self::Field => {
-                write!(f, "Database field error")
-            }
-            Self::SQL => {
-                write!(f, "Database SQL error")
-            }
-        }
-    }
-}
 
 #[cfg(test)]
 mod test {
--- crates/database/src/lib.rs
+++ crates/database/src/lib.rs
@@ -5,3 +5,5 @@
 #![warn(missing_docs, unreachable_pub, future_incompatible, rust_2018_idioms)]
 
 pub mod inventory;
+pub use error::Error;
+mod error;
